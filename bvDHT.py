eccfaac2f605c842f683df59fc92fe3d8b7180ec
Hailey Bistodeau <120686028+h-bistodeau@users.noreply.github.com>
2025-04-09 09:42:02 -0500

    Update bvDHT.py
    I fixed some small bugs within the initialization of the program. added '.listen()' to take in commands and made sure we had the ability to send commands as well.

--- bvDHT.py
+++ bvDHT.py
@@ -3,7 +3,7 @@ from sys import argv
 from socket import *
 import threading
 
-Valid_commands = ["locate", "connect", "disconnect", "contains","get", "insert", "remove"]
+Valid_commands = ["locate", "connect", "disconnect", "contains","get", "insert", "remove", "updatePrev"]
 hashTable = {} # The all-powerful hash table for this project.
 running = True
 
@@ -13,17 +13,17 @@ running = True
 # Focusing on starting the server.
 
 # Don't need a class for the finger table, just a dictionary. Key is whatever it is, value should be the peer address.
-def fingerTableSetup(self, startup = True):
+def fingerTableSetup(self_id, startup = True):
     table = {}
     if startup:
         for i in range(1,5):
-            table[f"finger{i}"] = self
-        table["next"] = self
-        table["prev"] = self
+            table[f"finger{i}"] = self_id
+        table["next"] = self_id
+        table["prev"] = self_id
     else:
         for i in range(1,5):
             table[f"finger{i}"] = "Test"
-    table["self"] = self
+    table["self"] = self_id
 
     return table
 
@@ -57,6 +57,8 @@ def handle_messages(socket):
 
                 elif str_msg == "remove":
                     print("recieved remove command.... Now running remove")
+                elif str_msg == "updatePrev":
+                    print("recieved updatePrev command.... Now running updatePrev")
 
             else:
                 print("recieved unknown command, or data is being recieved elsewhere")
@@ -73,6 +75,7 @@ if __name__ == "__main__":
     # If the length of argv is one (just the program), then we launch as the initial connection
 
     if len(argv) == 1:
+        # we are acting as the server in some regard and start everything up
         # server_start()
         sock.bind(('', 8008))
         sock.listen(1)
@@ -81,25 +84,42 @@ if __name__ == "__main__":
         conn, addr = sock.accept()
         print("Connection by", addr)
 
-        self = sock.getsockname()[0]
-        hashTable = fingerTableSetup(hash(self), True)
+        self_id = sock.getsockname()[0]
+        hashTable = fingerTableSetup(hash(self_id), True)
         print(hashTable)
 
-        #create thread to handle incoming msg/commands
+        #create thread to handle incoming msg/command
+        active_sock = conn
         threading.Thread(target=handle_messages, args=(conn,), daemon=True).start()
 
     # Otherwise, we connect to a peer.
-    elif len(argv) > 1:
+    else:
+        # this implies that the server has already been started and we connect to a peer within the DHT
         # connect_to_system()
-        hashTable = fingerTableSetup(hash(argv[1]), False)
+        peerIP = argv[1]
+        sock.connect((peerIP, 8008))
+
+        self_id = sock.getsockname()[0]
+        hashTable = fingerTableSetup(hash(self_id), False)
         print(hashTable)
-        threading.Thread(target=handle_messages, args=(sock,), daemon=True).start
-        
-    #super duper bare bones send commands loop (literally no cool features just the starting point)   
+
+        active_sock = sock
+        threading.Thread(target=handle_messages, args=(sock,), daemon=True).start()
+
+    #super duper bare bones send commands loop (literally no cool features just the starting point)
     while running:
         command = input("Enter command: ")
         if command not in Valid_commands:
             print("Invalid command")
-            pass
-        
-        sock.send(command.encode())
+            continue
+
+        try:
+            #attempts to send command to the peer that was initialized prior to this
+            active_sock.send(command.encode())
+
+        except KeyboardInterrupt:
+            print("shutting down the program")
+            running = False
+
+        except Exception as e:
+            print("there was an error, thats probably on me")
